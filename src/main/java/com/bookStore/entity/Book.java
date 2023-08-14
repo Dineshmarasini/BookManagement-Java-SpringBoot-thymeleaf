511f6d692dd07240062d1bdcfe13f2b360b18d69
Dineshmarasini <99908688+Dineshmarasini@users.noreply.github.com>
2023-08-14 23:55:53 +0545

    this is first

---
+++ src/main/java/com/bookStore/entity/Book.java
@@ -0,0 +1,59 @@
+package com.bookStore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Name;
+    private  String author;
+    private String price;
+
+    public Book(int id, String name, String author, String price) {
+        super();
+        this.id = id;
+        Name = name;
+        this.author = author;
+        this.price = price;
+    }
+    public Book(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
